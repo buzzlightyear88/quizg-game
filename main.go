69d5cd577c00a1f10107fa9d3698e388e25aaa74
Panos <panagiotis.rapo@gmail.com>
2019-05-27 16:06:44 -0400

    Added logging and flags

--- main.go
+++ main.go
@@ -1,6 +1,14 @@
 package main
 
-import "time"
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"time"
+)
 
 type Problem struct {
 	q string
@@ -22,7 +30,7 @@ var wrong int
 
 // parseProblems reads the problems from the file, line by line
 // sends problems into a channel
-func parseProblems() {
+func parseProblems(problems chan Problem, filename string, shuffle bool) {
 
 }
 
@@ -37,5 +45,41 @@ func startTimer(seconds int) {
 }
 
 func main() {
+	// parse command line flags
+	filenameFl := flag.String("f", "problems.csv", "name of file")
+	secondsFl := flag.Int("t", TIMELIMIT, "time to solve the quiz")
+	shuffleFl := flag.Bool("s", false, "shuffle or not the questions")
+	debugFl := flag.Bool("debug", false, "show debug information")
+
+	// discard debug data if not wanted
+	if !*debugFl {
+		log.SetOutput(ioutil.Discard)
+	}
+	// show logging information if flag is called
+	log.Println("debug:", *debugFl)
+	log.Println("filename:", *filenameFl)
+	log.Println("timer:", *secondsFl)
+	log.Println("shuffle: ", *shuffleFl)
+
+	// make a Problems channel
+	problems := make(chan Problem, PROBLEMCOUNT)
+
+	// read Problems goroutine
+	go parseProblems(problems, *filenameFl, *shuffleFl)
+
+	// prompt to start the quiz
+	fmt.Printf("Press any key to start the quiz!")
+	bufio.NewScanner(os.Stdin).Scan()
+
+	// solve Problems goroutine
+	go solveProblems(problems)
+
+	// start the timer
+	startTimer(*secondsFl)
+
+	// show final tally
+	fmt.Printf("\nNumber of questions: %d\n", count)
+	fmt.Printf("Correct answers: %d", correct)
+	fmt.Printf("Incorrect answers: %d", wrong)
 
 }
